f0c66e02651b88e47d40e024c25cf1007bab726c
zjfjack <zekejeff@gmail.com>
2020-06-24 00:26:37 +1000

    Add some Pawn examples

---
+++ Source/FirstProject/Collider.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Collider.generated.h"
+
+UCLASS()
+class FIRSTPROJECT_API ACollider : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACollider();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "MeshComponent")
+	UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "SphereComponent")
+	class USphereComponent* SphereComponent;
+
+	FORCEINLINE UStaticMeshComponent* GetMeshComponent() const { return MeshComponent; }
+	FORCEINLINE void SetMeshComponent(UStaticMeshComponent* Mesh) { MeshComponent = Mesh; }
+
+	FORCEINLINE USphereComponent* GetSphereComponent() const { return SphereComponent; }
+	FORCEINLINE void SetSphereComponent(USphereComponent* Sphere) { SphereComponent = Sphere; }
+};
---
+++ Source/FirstProject/Critter.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Critter.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+ACritter::ACritter() :
+	MeshComponent(CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"))),
+	CameraComponent(CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"))),
+	CurrentVelocity(FVector(0.f))
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+	MeshComponent->SetupAttachment(GetRootComponent());
+
+	CameraComponent->SetupAttachment(GetRootComponent());
+	CameraComponent->SetRelativeLocation(FVector(-300.f, 0.f, 300.f));
+	CameraComponent->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
+
+	// AutoPossessPlayer = EAutoReceiveInput::Player0;
+}
+
+// Called when the game starts or when spawned
+void ACritter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACritter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	FVector NewLocation = GetActorLocation() + CurrentVelocity * DeltaTime;
+	SetActorLocation(NewLocation);
+
+	UE_LOG(LogTemp, Warning, TEXT("NewLocation: %s"), *NewLocation.ToString());
+}
+
+// Called to bind functionality to input
+void ACritter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ACritter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &ACritter::MoveRight);
+}
+
+void ACritter::MoveForward(float Value)
+{
+	CurrentVelocity.X = FMath::Clamp(Value, -1.f, 1.f) * MaxSpeed;
+}
+
+void ACritter::MoveRight(float Value)
+{
+	CurrentVelocity.Y = FMath::Clamp(Value, -1.f, 1.f) * MaxSpeed;
+}
+
