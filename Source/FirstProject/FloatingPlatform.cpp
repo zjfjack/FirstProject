af977ce7b62b3a63ad69955f8be1debedc38fed8
zjfjack <zekejeff@gmail.com>
2020-06-28 21:55:24 +1000

    Add floating platform with VInterpTo implementation

---
+++ Source/FirstProject/FloatingPlatform.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FloatingPlatform.h"
+
+// Sets default values
+AFloatingPlatform::AFloatingPlatform() :
+	Mesh(CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"))),
+	StartPoint(0.f),
+	EndPoint(0.f)
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SetRootComponent(Mesh);
+}
+
+// Called when the game starts or when spawned
+void AFloatingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	StartPoint = GetActorLocation();
+	// EndPoint is relative location
+	EndPoint += StartPoint;
+}
+
+// Called every frame
+void AFloatingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector CurrentLocation = GetActorLocation();
+	if ((CurrentLocation - EndPoint).Size() < 1.f)
+	{
+		// Stop moving
+		ToggleIsInterping();
+		GetWorldTimerManager().SetTimer(InterpTimer, this, &AFloatingPlatform::ToggleIsInterping, InterpTime);
+		Swap(StartPoint, EndPoint);
+	}
+	
+	FVector Interp = FMath::VInterpTo(CurrentLocation, EndPoint, DeltaTime, InterpSpeed);
+	SetActorLocation(Interp);
+}
+
+void AFloatingPlatform::ToggleIsInterping()
+{
+	bIsInterping = !bIsInterping;
+}
---
+++ Source/FirstProject/FloatingPlatform.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FloatingPlatform.generated.h"
+
+UCLASS()
+class FIRSTPROJECT_API AFloatingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFloatingPlatform();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Platform")
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditAnywhere)
+	FVector StartPoint;
+
+	UPROPERTY(EditAnywhere, Category = "Platform",  meta = (MakeEditWidget = "true"))
+	FVector EndPoint;
+
+	UPROPERTY(EditAnywhere, Category = "Platform")
+	float InterpSpeed = 4.f;
+
+	FTimerHandle InterpTimer;
+
+	bool bIsInterping = true;
+
+	UPROPERTY(EditAnywhere, Category = "Platform")
+	float InterpTime = 2.f;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void ToggleIsInterping();
+};
